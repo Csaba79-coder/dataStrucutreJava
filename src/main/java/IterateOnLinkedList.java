c1e2581471e87fa6b9ecf26e5f3726d56e633219
Csaba79-coder <csabavadasz79@gmail.com>
2022-08-12 11:28:03 +0200

    Lists

--- src/main/java/IterateOnLinkedList.java
+++ src/main/java/IterateOnLinkedList.java
@@ -15,7 +15,13 @@ public class IterateOnLinkedList {
      */
 
     public static void main(String[] args) {
+        // https://docs.oracle.com/javase/6/docs/api/java/util/List.html
 
+        /*
+        An ordered collection (also known as a sequence). The user of this interface has precise control over where in
+        the list each element is inserted. The user can access elements by their integer index (position in the list),
+        and search for elements in the list.
+         */
     }
 
     public static void IterateWithForLoop() {
